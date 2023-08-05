5aaf19786e483672d11631bae0697a0b506a1c68
Tudolla <nguyenvantu.cnpm@gmail.com>
2023-08-05 22:16:51 +0700

    commit first

---
+++ lib/consts/strings.dart
@@ -0,0 +1,12 @@
+const appname = "We market";
+const appversion = "Version 1.0.0";
+const credits = "@Tudev PTIT";
+const email = "Email";
+const emailHint = 'ronando@gmail.com';
+const password = "Password";
+const passHint = "********";
+const forgerPass = "Forget Password!";
+const login = "Login";
+const signup = "Sign up";
+const createNewAccount = "or, create a new account";
+const loginWith = 'Or, Login with';
